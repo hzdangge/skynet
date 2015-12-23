8869c3505514f10b58aa046d8f2a96717c4154d5
xiefan <xiexie525@qq.com>
2015-12-23 20:47:52 +0800

    用于共享数据的 双端队列

---
+++ lualib-src/lua-deque.c
@@ -0,0 +1,153 @@
+#include "skynet_malloc.h"
+#include "spinlock.h"
+
+#include <lua.h>
+#include <lauxlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+struct node
+{
+	uint32_t elem;
+	struct node *next;
+};
+
+struct deque {
+	struct spinlock lock;
+	struct node *head;
+	struct node *tail;
+	uint32_t size;
+};
+
+#define checkdeque(L) \
+	(struct deque *)luaL_checkudata(L, 1, "dangge.deque")
+
+int
+lpush(lua_State *L) {
+	struct deque *q = checkdeque(L);
+	SPIN_LOCK(q)
+	if (q == NULL) {
+		SPIN_UNLOCK(q)
+		return 0;
+	}
+	uint32_t elem = luaL_checkinteger(L, 2) & 0xFFFFFFFF;
+	struct node *pd = skynet_malloc(sizeof(*pd));
+	pd->elem = elem;
+	pd->next = NULL;
+	if(q->tail) {
+		q->tail->next = pd;
+		q->tail = pd;
+	} else {
+		q->head = q->tail = pd;
+	}
+	q->size += 1;
+	SPIN_UNLOCK(q)
+	return 0;
+}
+
+uint32_t
+_pop(struct deque *q) {
+	struct node *pd = q->head;
+	if(pd) {
+		q->head = pd->next;
+		if(q->head == NULL) {
+			assert(pd == q->tail);
+			q->tail = NULL;
+		}
+		pd->next = NULL;
+		q->size -= 1;
+		uint32_t rtn = pd->elem;
+		skynet_free(pd);
+		return rtn;
+	}
+	return 0;
+}
+
+int
+lpop(lua_State *L) {
+	struct deque *q = checkdeque(L);
+	SPIN_LOCK(q)
+	if (q) {
+		uint32_t elem = _pop(q);
+		if (elem > 0) {
+			lua_pushinteger(L, elem);
+			SPIN_UNLOCK(q)
+			return 1;
+		}
+	}
+	SPIN_UNLOCK(q)
+	return 0;
+}
+
+int
+lgc(lua_State *L) {
+	struct deque *q = checkdeque(L);
+	SPIN_LOCK(q)
+	if (q) {
+		for (;_pop(q) != 0;);
+
+	}
+	SPIN_UNLOCK(q)
+	SPIN_DESTROY(q)
+	skynet_free(q);
+	return 0;
+}
+
+int
+lnew(lua_State *L) {
+	struct deque *q = skynet_malloc(sizeof(*q));
+	memset(q, 0, sizeof(*q));
+	lua_pushlightuserdata(L, q);
+	luaL_getmetatable(L, "dangge.deque");
+	lua_pushvalue(L, -1);
+	lua_pushcfunction(L, lgc);
+	lua_setfield(L, -2, "__gc");
+	lua_setmetatable(L, 1);
+	lua_pop(L, 1);
+	SPIN_INIT(q)
+	return 1;
+}
+
+int
+lclone(lua_State *L) {
+	void *q = lua_touserdata(L, 1);
+	if (q == NULL) return 0;
+	luaL_getmetatable(L, "dangge.deque");
+	lua_setmetatable(L, -2);
+	return 1;
+}
+
+int
+lsize(lua_State *L) {
+	struct deque *q = checkdeque(L);
+	SPIN_LOCK(q)
+	if (q) {
+		lua_pushinteger(L, q->size);
+	} else lua_pushinteger(L, 0);
+	SPIN_UNLOCK(q)
+	return 1;
+}
+
+int
+luaopen_deque(lua_State *L) {
+	luaL_Reg m[] = {
+		{ "pop", lpop },
+		{ "push", lpush },
+		{ "size", lsize },
+		{ NULL, NULL },
+	};
+	luaL_newmetatable(L, "dangge.deque");
+	lua_pushvalue(L, -1);
+	lua_setfield(L, -2, "__index");
+	luaL_setfuncs(L, m, 0);
+
+	luaL_Reg l[] = {
+		{ "new", lnew },
+		{ "clone", lclone },
+		{ NULL, NULL },
+	};
+	luaL_newlib(L, l);
+	return 1;
+}
---
+++ test/testdeque.lua
@@ -0,0 +1,45 @@
+local skynet = require "skynet"
+local deque = require "deque"
+
+local mode = ...
+
+local timestr = tostring(math.floor(skynet.time()))
+math.randomseed(tonumber(timestr:sub(1,6)))
+
+if mode == "slave" then
+
+skynet.start(function()
+	skynet.dispatch("lua", function (session, source, obj)
+		if session == 0 then
+			skynet.exit()
+			return
+		end
+		local obj = deque.clone(obj)
+		skynet.ret()
+
+		while true do
+			skynet.sleep(math.random(2))
+			skynet.error("read:", obj:pop())
+		end
+	end)
+end)
+
+else
+
+skynet.start(function()
+	local slave = skynet.newservice(SERVICE_NAME, "slave")
+
+	local obj = deque.new()
+
+	skynet.call(slave, "lua", obj)
+
+	for i=1,1000 do
+		skynet.sleep(math.random(2))
+		obj:push(i)
+		skynet.error("write", i, obj:size())
+	end
+
+	skynet.send(slave, "lua", "close")
+end)
+
+end
