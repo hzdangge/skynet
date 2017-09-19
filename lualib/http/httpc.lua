456fbd8f9e9a9410ab9c61107eac382a9d9d1ac9
zhengshouren <zhengshouren@qq.com>
2017-09-19 15:40:17 +0800

    1.1.0-rc

--- lualib/http/httpc.lua
+++ lualib/http/httpc.lua
@@ -1,7 +1,8 @@
+local skynet = require "skynet"
 local socket = require "http.sockethelper"
 local url = require "http.url"
 local internal = require "http.internal"
-local dns = require "dns"
+local dns = require "skynet.dns"
 local string = string
 local table = table
 
@@ -72,7 +73,8 @@ local function request(fd, method, host, url, recvheader, header, content)
 				body = body .. padding
 			end
 		else
-			body = nil
+			-- no content-length, read all
+			body = body .. socket.readall(fd)
 		end
 	end
 
@@ -87,6 +89,7 @@ function httpc.dns(server,port)
 end
 
 function httpc.request(method, host, url, recvheader, header, content)
+	local timeout = httpc.timeout	-- get httpc.timeout before any blocked api
 	local hostname, port = host:match"([^:]+):?(%d*)$"
 	if port == "" then
 		port = 80
@@ -96,9 +99,22 @@ function httpc.request(method, host, url, recvheader, header, content)
 	if async_dns and not hostname:match(".*%d+$") then
 		hostname = dns.resolve(hostname)
 	end
-	local fd = socket.connect(hostname, port)
+	local fd = socket.connect(hostname, port, timeout)
+	local finish
+	if timeout then
+		skynet.timeout(timeout, function()
+			if not finish then
+				local temp = fd
+				fd = nil
+				socket.close(temp)
+			end
+		end)
+	end
 	local ok , statuscode, body = pcall(request, fd,method, host, url, recvheader, header, content)
-	socket.close(fd)
+	finish = true
+	if fd then	-- may close by skynet.timeout
+		socket.close(fd)
+	end
 	if ok then
 		return statuscode, body
 	else
--- service-src/service_logger.c
+++ service-src/service_logger.c
@@ -3,15 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-
 #include <string.h>
-#include <time.h>
-#if defined(__APPLE__)
-	#include <sys/time.h>
-#endif
 
 struct logger {
 	FILE * handle;
+	char * filename;
 	int close;
 };
 
@@ -20,6 +16,8 @@ logger_create(void) {
 	struct logger * inst = skynet_malloc(sizeof(*inst));
 	inst->handle = NULL;
 	inst->close = 0;
+	inst->filename = NULL;
+
 	return inst;
 }
 
@@ -28,16 +26,26 @@ logger_release(struct logger * inst) {
 	if (inst->close) {
 		fclose(inst->handle);
 	}
+	skynet_free(inst->filename);
 	skynet_free(inst);
 }
 
 static int
-_logger(struct skynet_context * context, void *ud, int type, int session, uint32_t source, const void * msg, size_t sz) {
+logger_cb(struct skynet_context * context, void *ud, int type, int session, uint32_t source, const void * msg, size_t sz) {
 	struct logger * inst = ud;
-	fprintf(inst->handle, "[:%08x] ",source);
-	fwrite(msg, sz , 1, inst->handle);
-	fprintf(inst->handle, "\n");
-	fflush(inst->handle);
+	switch (type) {
+	case PTYPE_SYSTEM:
+		if (inst->filename) {
+			inst->handle = freopen(inst->filename, "a", inst->handle);
+		}
+		break;
+	case PTYPE_TEXT:
+		fprintf(inst->handle, "[:%08x] ",source);
+		fwrite(msg, sz , 1, inst->handle);
+		fprintf(inst->handle, "\n");
+		fflush(inst->handle);
+		break;
+	}
 
 	return 0;
 }
@@ -45,24 +53,18 @@ _logger(struct skynet_context * context, void *ud, int type, int session, uint32
 int
 logger_init(struct logger * inst, struct skynet_context *ctx, const char * parm) {
 	if (parm) {
-		if (!strchr(parm, '.')) {
-			char tmp1[64], tmp2[128];
-			time_t t = time(NULL);
-			strftime(tmp1, 64, "%Y-%m%d-%H%M", localtime(&t));
-			sprintf(tmp2, "%s-%s", parm, tmp1);
-			inst->handle = fopen(tmp2, "w");
-		} else {
-			inst->handle = fopen(parm, "w");
-		}
+		inst->handle = fopen(parm,"w");
 		if (inst->handle == NULL) {
 			return 1;
 		}
+		inst->filename = skynet_malloc(strlen(parm)+1);
+		strcpy(inst->filename, parm);
 		inst->close = 1;
 	} else {
 		inst->handle = stdout;
 	}
 	if (inst->handle) {
-		skynet_callback(ctx, inst, _logger);
+		skynet_callback(ctx, inst, logger_cb);
 		skynet_command(ctx, "REG", ".logger");
 		return 0;
 	}
